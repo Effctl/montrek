0db235083d0b0350f5504ef0a59668de81dfd2b4
jame86 <jan.mellert@effctl.com>
2026-05-06 23:22:16 +0200

    Fix issues with wheel implementation

--- bin/local/sync-python-env.sh
+++ bin/local/sync-python-env.sh
@@ -4,6 +4,10 @@ set -x
 
 echo "Syncing Python environment with uv..."
 
+# Get the project root directory
+PROJECT_ROOT="$(git rev-parse --show-toplevel)"
+echo "PROJECT_ROOT=${PROJECT_ROOT}"
+
 # Create .venv only if it does not already exist.
 # This avoids permission errors when stale files exist in an existing venv.
 if [ ! -d ".venv" ]; then
@@ -15,7 +19,22 @@ fi
 # Combine all requirements.in files into one
 temporary_requirements_file="all_requirements.in"
 >"$temporary_requirements_file"
-find . -name 'requirements.in' -exec sh -c 'cat "$1"; echo' _ {} \; >>"$temporary_requirements_file"
+
+# Replace relative paths with .whl with absolute path based on the project root
+
+while IFS= read -r line; do
+  line="${line%$'\r'}"
+  if [[ "$line" == *.whl ]] && [[ "$line" !=  /* ]]; then
+    new_path="$PROJECT_ROOT/$line"
+    echo "Rewriting wheel path: '$line' -> '$new_path'"
+    echo "$PROJECT_ROOT/$line" >> "$temporary_requirements_file"
+    echo "$new_path" >> "$temporary_requirements_file"
+  else
+    echo "$line" >> "$temporary_requirements_file"
+  fi
+done < <(find . -name 'requirements.in' -exec cat {} +)
+
+# find . -name 'requirements.in' -exec sh -c 'cat "$1"; echo' _ {} \; >>"$temporary_requirements_file"
 
 # Compile and sync using uv
 uv pip compile "$temporary_requirements_file" --output-file requirements.txt
